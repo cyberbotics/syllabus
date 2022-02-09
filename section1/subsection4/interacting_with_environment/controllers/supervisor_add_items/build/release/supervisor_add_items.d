9a13fcd6e0aff63ccb12365c9422304dfe45a2ad
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-02-09 09:52:01 +0100

    robot window environment

--- section1/subsection4/interacting_with_environment/controllers/supervisor_add_items/build/release/supervisor_add_items.d
+++
@@ -1,7 +0,0 @@
-build/release/supervisor_add_items.o: supervisor_add_items.c \
- /home/benjamin/webots2/include/controller/c/webots/supervisor.h \
- /home/benjamin/webots2/include/controller/c/webots/contact_point.h \
- /home/benjamin/webots2/include/controller/c/webots/nodes.h \
- /home/benjamin/webots2/include/controller/c/webots/types.h \
- /home/benjamin/webots2/include/controller/c/webots/robot.h \
- /home/benjamin/webots2/include/controller/c/webots/keyboard.h
