5eb6747d71e4ead4f11975081e9a8a782fa36e04
joachimhgg <joachim.honegger@gmail.com>
2022-02-17 16:28:24 +0100

    add make

--- section1/subsection4/interacting_with_environment/plugins/robot_windows/interact_with_environment/build/release/interact_with_environment.d
+++
@@ -1,7 +0,0 @@
-build/release/interact_with_environment.o: interact_with_environment.c \
- /home/benjamin/webots2/include/controller/c/webots/plugins/robot_window/robot_wwi.h \
- /home/benjamin/webots2/include/controller/c/webots/robot.h \
- /home/benjamin/webots2/include/controller/c/webots/types.h \
- /home/benjamin/webots2/include/controller/c/webots/nodes.h \
- /home/benjamin/webots2/include/controller/c/webots/supervisor.h \
- /home/benjamin/webots2/include/controller/c/webots/contact_point.h
