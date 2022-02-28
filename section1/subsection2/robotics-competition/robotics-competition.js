10edd107fbb269ff710f03f1c14084ec93922bbe
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-02-28 11:11:45 +0100

    improve css

--- section1/subsection2/robotics-competition/robotics-competition.js
+++ section1/subsection2/robotics-competition/robotics-competition.js
@@ -1,4 +1,4 @@
-import WebotsView from "https://cyberbotics.com/wwi/R2022b/WebotsView.js";
+import WebotsView from 'https://cyberbotics.com/wwi/R2022b/WebotsView.js';
 
 let webotsView = new WebotsView();
 document.body.appendChild(webotsView);
@@ -11,28 +11,26 @@ webotsView.showStep = false;
 let showRobotWindow = true;
 
 function displayRobotWindow() {
-
   if (webotsView._view && webotsView._view.x3dScene && webotsView._view.x3dScene.prefix) {
-
-    let newDiv = document.createElement("div");
-    newDiv.className = "arrow-left";
-    newDiv.id = "arrow";
-    let menuButton = document.createElement("button")
-    menuButton.className = "menu-button";
-    menuButton.title = "Show/Hide the robot window.";
+    let newDiv = document.createElement('div');
+    newDiv.className = 'arrow-left';
+    newDiv.id = 'arrow';
+    let menuButton = document.createElement('button');
+    menuButton.className = 'menu-button';
+    menuButton.title = 'Show/Hide the robot window.';
 
     document.body.appendChild(menuButton);
     menuButton.appendChild(newDiv);
 
     if (document.getElementsByClassName('menu-button').length !== 0)
-        document.getElementsByClassName('menu-button')[0].onclick = () => toggleRobotWindow();
+      document.getElementsByClassName('menu-button')[0].onclick = () => toggleRobotWindow();
 
     let serverUrl = webotsView._view.x3dScene.prefix;
     let url = serverUrl + 'robot_windows/competition_robot_window/competition_robot_window.html?name=robot';
     let iframe = document.createElement('iframe');
     iframe.src = url;
     iframe.id = 'iframe';
-    document.body.appendChild(iframe)
+    document.body.appendChild(iframe);
 
     if (document.getElementById('fullscreenButton'))
       document.getElementById('fullscreenButton').onclick = () => toggleRobotComponentFullScreen();
@@ -41,9 +39,11 @@ function displayRobotWindow() {
       document.getElementById('partscreenButton').style.display = 'none';
     }
     autoPlay();
-  } else setTimeout(function () {
-    displayRobotWindow()
-  }, 100);
+  } else {
+    setTimeout(function() {
+      displayRobotWindow();
+    }, 100);
+  }
 }
 
 function toggleRobotComponentFullScreen(robot) {
@@ -56,9 +56,8 @@ function toggleRobotComponentFullScreen(robot) {
   } else {
     document.getElementById('fullscreenButton').style.display = 'none';
     document.getElementById('partscreenButton').style.display = '';
-    if (document.body.requestFullscreen) {
+    if (document.body.requestFullscreen)
       document.body.requestFullscreen();
-    }
   }
 }
 
@@ -86,13 +85,15 @@ function updateRobotComponentDimension() {
   const iframe = document.getElementById('iframe');
   if (document.getElementsByClassName('menu-button').length !== 0) {
     if (showRobotWindow === true) {
-        iframe.style.display = '';
-        webotsView.style.width = 'calc(100% - 350px)';
-            document.getElementsByClassName('menu-button')[0].style.left = '350px';
+      iframe.style.display = '';
+      webotsView.style.width = 'calc(100% - 350px)';
+      webotsView.style.left = '350px';
+      document.getElementsByClassName('menu-button')[0].style.left = '350px';
     } else {
-        iframe.style.display = 'none';
-        webotsView.style.width = '100%';
-        document.getElementsByClassName('menu-button')[0].style.left = '0px';
+      iframe.style.display = 'none';
+      webotsView.style.width = '100%';
+      webotsView.style.left = '0px';
+      document.getElementsByClassName('menu-button')[0].style.left = '0px';
     }
   }
   webotsView.resize();
