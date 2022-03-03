dd9e7278c18b95e4f8dada90f1934177444e9355
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-03-03 16:34:01 +0100

    distance_sensor

--- section2/2StartUpProcedure/controllers/ure_can_grasper_python_complete/ure_can_grasper_python.py
+++ section2/2StartUpProcedure/controllers/ure_can_grasper_python_complete/ure_can_grasper_python.py
@@ -40,7 +40,7 @@ shoulder_rotation = robot.getDevice('shoulder_pan_joint')
 
 shoulder_rotation.setPosition(1.5708)
 
-distance_sensor = robot.getDevice('distance sensor')
+distance_sensor = robot.getDevice('distance_sensor')
 distance_sensor.enable(TIME_STEP)
 
 position_sensor = robot.getDevice('wrist_1_joint_sensor')
