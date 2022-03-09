a36002355733cc59b265e81ee1d6addb9eb0c58c
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-03-09 14:12:24 +0100

    end instructions4

--- section2/4PositionOfRobot/controllers/robot_controller/robot_controller.py
+++ section2/4PositionOfRobot/controllers/robot_controller/robot_controller.py
@@ -32,20 +32,3 @@ centralRightSensor.enable(TIME_STEP)
 outerRightSensor.enable(TIME_STEP)
 
 #################### WRITE YOUR CODE HERE ###########################
-
-leftMotor.setPosition(float('inf'))
-rightMotor.setPosition(float('inf'))
-
-leftMotor.setVelocity(3)
-rightMotor.setVelocity(3)
-
-while robot.step(TIME_STEP) != -1:
-    outerLeftSensorValue = outerLeftSensor.getValue() / distanceSensorCalibrationConstant
-    centralLeftSensorValue = centralLeftSensor.getValue() / distanceSensorCalibrationConstant
-    centralSensorValue = centralSensor.getValue() / distanceSensorCalibrationConstant
-    centralRightSensorValue = centralRightSensor.getValue() / distanceSensorCalibrationConstant
-    outerRightSensorValue = outerRightSensor.getValue() / distanceSensorCalibrationConstant
-
-    # Set wheel velocities based on sensor values, prefer right turns if the central sensor is triggered.
-    leftMotor.setVelocity(3 - (centralRightSensorValue + outerRightSensorValue) / 2)
-    rightMotor.setVelocity(3 - (centralLeftSensorValue + outerLeftSensorValue) / 2 - centralSensorValue)
