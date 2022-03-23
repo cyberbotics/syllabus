ed086e602588b6a815990de3b08e9c4a43ee48ed
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-03-23 11:47:15 +0100

    military first version

--- section3/Military/controllers/military_robot/military_robot.py
+++ section3/Military/controllers/military_robot/military_robot.py
@@ -2,37 +2,38 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Supervisor
+from controller import Robot, Motor
 
 # create the Robot instance.
-robot = Supervisor()
+robot = Robot()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
+leftMotor = robot.getDevice('left motor');
+rightMotor = robot.getDevice('right motor');
 
-node = robot.getFromDef("ROBOT")
+turretMotor = robot.getDevice('turret_motor');
+gunsMotor = robot.getDevice('guns_motor');
+gunsMotor.setPosition(0.45)
 
-field1 = node.getFieldByIndex(0);
-print(field1.getName())
-field2 = node.getFieldByIndex(2);
-print(field2.getName())
-field3 = node.getFieldByIndex(1);
-print(field3.getName())
-field4 = node.getFieldByIndex(4);
-print(field4.getName())
-field5 = node.getFieldByIndex(3);
-print(field5.getName())
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
 
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
+leftMotor.setVelocity(1)
+rightMotor.setVelocity(1)
 
-    # Process sensor data here.
+timeStep = robot.getBasicTimeStep();
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+robot.step(13500)
 
+leftMotor.setVelocity(0.5)
 
+robot.step(4000)
+
+leftMotor.setVelocity(0)
+rightMotor.setVelocity(0)
+turretMotor.setPosition(0.93)
+
+robot.step(500)
+gunsMotor.setPosition(-0.05)
